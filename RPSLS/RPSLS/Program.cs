9e83e40a6f855ff547467b68b1bfe5f9a8c3bc5c
StevenRandallVolbrecht <svolbrecht@yahoo.com>
2018-08-24 08:12:11 -0500

    intial RPSLS file upload

---
+++ RPSLS/RPSLS/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.ReadKey();
+
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+    }
+}
